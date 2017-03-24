eefc58edba1c1ca403084544473428bf42ad6bb0
wax8280 <wax8280@163.com>
2017-03-24 11:50:28 +0800

    fix bug

--- zipit.py
+++ zipit.py
@@ -5,6 +5,15 @@ from zipit.setting import *
 import datetime
 
 
+# datetime.datetime.now().strftime("%Y-%m-%d-%H")
+# '2017-03-24-10'
+
+# datetime.datetime.strptime('2017-03-24-10', "%Y-%m-%d-%H")
+# datetime.datetime(2017, 3, 24, 10, 0)
+
+# datetime.datetime.strptime('2017-03-24-10', "%Y-%m-%d-%H") - datetime.timedelta(hours=1)
+# datetime.datetime(2017, 3, 24, 9, 0)
+
 def start_zip(absolute_dir):
     print('Start zip {}'.format(absolute_dir))
     output = run_command('7za a -r {} {}'.format(absolute_dir, absolute_dir))
@@ -15,13 +24,13 @@ def start_zip(absolute_dir):
         print('Zip failed!')
         print(output)
 
-
 def zip_dir(path, filter_dirname_list=None):
     all_dir = search_dir(path, filter_dirname_list)
-    now_date = datetime.datetime.now().date()
+    now_date_string = datetime.datetime.now().strftime("%Y-%m-%d-%H")
+    now_date = datetime.datetime.strptime(now_date_string, "%Y-%m-%d-%H")
 
     for each_dir in all_dir:
-        dir_date = datetime.datetime.strptime(each_dir, "%Y-%m-%d").date()
+        dir_date = datetime.datetime.strptime(each_dir, "%Y-%m-%d-%H")
         if now_date > dir_date:
             absolute_dir = os.path.join(path, each_dir)
             start_zip(absolute_dir)
